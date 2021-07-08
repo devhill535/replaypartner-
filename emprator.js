b2794dafcd35b4c7c2f29f6191be78cf53d25dc8
Glitch (geode-harsh-ziconium) <none>
2021-07-08 15:36:59 +0000

    🌂💑 Updated with Glitch

--- emprator.js
+++ emprator.js
@@ -28,11 +28,7 @@ client.on("ready", () => {
   console.log(
     `Online In Servers : ${client.guilds.size} | Users : ${client.users.size}`
   );
-  let statuses = [
-    `Servers: ${client.guilds.size} | Users: ${client.users.size}`,
-    `PARTNER BOT `,
-    `BY : SAEWAN`
-  ];
+  let statuses = [`PARTNER BOT `, `BY : HELL`];
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
     client.user.setActivity(STREAMING, {
@@ -51,6 +47,40 @@ client.on("message", SAEWAN => {
   }
 });
 
+client.on("message", SAEWAN => {
+  if (SAEWAN.content.startsWith("Reklam")) {
+    SAEWAN.author.send("**چاوەکەم ریـکـلامـەکـەم بـۆ بنێرە**");
+    SAEWAN.reply("**ریـکلامەکەم لە تایـبـەت بـۆ بنێرە**");
+  }
+});
+
+client.on("message", SAEWAN => {
+  if (SAEWAN.content.startsWith("Slaw reklam")) {
+    SAEWAN.author.send("**چاوەکەم ریـکـلامـەکـەم بـۆ بنێرە**");
+    SAEWAN.reply("**ریـکلامەکەم لە تایـبـەت بـۆ بنێرە**");
+  }
+});
+
+client.on("message", SAEWAN => {
+  if (SAEWAN.content.startsWith("slaw reklam")) {
+    SAEWAN.author.send("**چاوەکەم ریـکـلامـەکـەم بـۆ بنێرە**");
+    SAEWAN.reply("**ریـکلامەکەم لە تایـبـەت بـۆ بنێرە**");
+  }
+});
+
+client.on("message", SAEWAN => {
+  if (SAEWAN.content.startsWith("Slaw Reklam")) {
+    SAEWAN.author.send("**چاوەکەم ریـکـلامـەکـەم بـۆ بنێرە**");
+    SAEWAN.reply("**ریـکلامەکەم لە تایـبـەت بـۆ بنێرە**");
+  }
+});
+
+client.on("message", SAEWAN => {
+  if (SAEWAN.content.startsWith("سڵاو ڕیکلام")) {
+    SAEWAN.author.send("**چاوەکەم ریـکـلامـەکـەم بـۆ بنێرە**");
+    SAEWAN.reply("**ریـکلامەکەم لە تایـبـەت بـۆ بنێرە**");
+  }
+});
 ////////
 
 client.on("message", SAEWAN => {
@@ -63,7 +93,7 @@ client.on("message", SAEWAN => {
 // ======== { • REKLAM • }======== //
 
 client.on("ready", () => {
-  var join = client.channels.get("776849362642272277"); /// ئاید ڤۆیس
+  var join = client.channels.get("849717307050950666"); /// ئاید ڤۆیس
   if (join) join.join();
 });
 
@@ -71,10 +101,11 @@ client.on("message", saewan => {
   if (saewan.channel.type === "dm") {
     if (saewan.content.startsWith("https://discord.gg/")) {
       saewan.author.send(`> **تـۆش ریکلام بۆ ئـەم سێرڤـەرە بکە **  
-https://discord.gg/9DBUNV7`); /// لینکە سیرڤەرە خۆت
+https://discord.gg/uHbXwXSH6S`); /// لینکە سیرڤەرە خۆت
 
-      client.channels.get("760103301575344148").send(////ئایدی ژوری تێکست
- 
+      client.channels.get("849717278504386590").send(
+        ////ئایدی ژوری تێکست
+
         `> Nerdra la layan <@${saewan.author.id}> 
 ${saewan.content}`
       );
@@ -84,5 +115,5 @@ ${saewan.content}`
 
 // ======== { • TOKENN • }======== //
 
-client.login("لێرە تۆکین دانە");
+client.login("ODAwNzY1ODgwNTY4OTA1NzY5.YAW5Vw.9BuZuRNdQKeG7tyQLSiMIeCFiIM");
 ////////////////تکایە دەسکاری ئێرەمەکە تا ریمێکس دەیکت
